108b97ed749aaf3b5e750f22c81e0ba24d84c846
= <=>
2021-02-06 09:24:25 +0800

    refactor: 重构 Calendar 组件

--- src/components/calendar/index.vue
+++ src/components/calendar/index.vue
@@ -18,20 +18,20 @@
         </th>
       </thead>
       <tbody>
-        <tr v-for='(_, rowIndex) in Array(rowNum)' :key='rowIndex'>
+        <tr v-for='(itemArr, rowIndex) in calendar' :key='rowIndex'>
           <td
-            v-for='(_, columnIndex) in Array(columnNum)'
+            v-for='(date, columnIndex) in itemArr'
             :key='columnIndex'
             :class='{
-              prev: isPrevMonth(rowIndex, columnIndex),
-              next: isNextMonth(rowIndex, columnIndex),
-              active: activeIndex.rowIndex === rowIndex && activeIndex.columnIndex === columnIndex
+              prev: date.prev,
+              next: date.next,
+              active: date.current && calendarActiveDay === date.day,
             }'
             :style='{height: itemHeight + "px"}'
-            @click='handleOnClick(rowIndex, columnIndex)'
+            @click='handleOnClick(date)'
           >
             <div class='ki-calendar-day'>
-              {{computedCurrentDate(rowIndex, columnIndex)}}
+              {{date.day}}
             </div>
           </td>
         </tr>
@@ -48,9 +48,13 @@ import {
   reactive,
 } from 'vue';
 
-export type ActiveIndex = {
-  rowIndex?: number;
-  columnIndex?: number;
+export interface CalendarDateItem {
+  year?: number;
+  month?: number;
+  day?: number;
+  next?: boolean;
+  prev?: boolean;
+  current?: boolean;
 }
 
 export default defineComponent({
@@ -71,8 +75,10 @@ export default defineComponent({
       date: new Date(),
       year: new Date().getFullYear(),
       month: new Date().getMonth() + 1,
+      day: new Date().getDate(),
     });
-    const activeIndex = reactive<ActiveIndex>({});
+    const calendar = reactive<Array<CalendarDateItem>[]>([]);
+    const calendarActiveDay = ref<number>();
 
     /**
      * 获取当前月份的最后一天和当前月份第一天是星期几
@@ -87,15 +93,21 @@ export default defineComponent({
     };
 
     /**
-     * 计算页面展示的日期
+     * 生成日历每一项
      * */
-    const computedCurrentDate = (rowIndex: number, columnIndex: number) => {
+    const generateCalendarItem = (rowIndex: number, columnIndex: number): CalendarDateItem => {
       const date = columnIndex + 1 + (rowIndex * 7) - (currentMonthFirstDayWeek.value - 1);
+      const item: CalendarDateItem = {
+        year: currentDate.year,
+        month: currentDate.month,
+        day: date,
+      };
       if (rowIndex === 0) {
         if (columnIndex + 1 >= currentMonthFirstDayWeek.value) {
-          return date;
+          item.current = true;
+          return item;
         }
-        let last = 0; // 最后一天
+        let last: number; // 上个月最后一天
         if (currentDate.month === 1) {
           // 去年
           last = new Date(currentDate.year - 1, 12, 0).getDate();
@@ -105,21 +117,42 @@ export default defineComponent({
         }
         // (currentMonthFirstDayWeek.value - 1): 减一后的值才为该填充的数量
         // (columnIndex + 1): index 从 0 开始，所以要加 1
-        return last - (currentMonthFirstDayWeek.value - 1) + (columnIndex + 1);
+        item.prev = true;
+        item.day = last - (currentMonthFirstDayWeek.value - 1) + (columnIndex + 1);
+        return item;
       }
       if (date <= lastDay.value) {
-        return date;
+        item.current = true;
+        return item;
       }
+      item.next = true;
       // 总的表格数量 - ( 当前月的天数 + 第一天因为星期偏移的值 ) = 下个月在表格中展示的天数
       const next = (rowNum * columnNum) - (lastDay.value + (currentMonthFirstDayWeek.value - 1));
       const diff = (next - columnNum); // 倒数第二行需要填充的数量
       const secondLast = columnNum - diff; // 倒数第二行已经填充的数量
       if (rowIndex === 4) {
         // 当前列的 index - 已经填充的数量 + 1 = 实际需要在倒数第二行需要填充的下一个月的值
-        return columnIndex - secondLast + 1;
+        item.day = columnIndex - secondLast + 1;
+        return item;
       }
       // 倒数第一行则需要加上 diff
-      return columnIndex + diff + 1;
+      item.day = columnIndex + diff + 1;
+      return item;
+    };
+
+    /**
+     * 生成日历
+     * */
+    const generateCalendar = () => {
+      getCurrentMonthLastDayAndFirstDayWeek(currentDate.year, currentDate.month);
+      calendar.splice(0, calendar.length); // 清空原数组
+      for (let rowIndex = 0; rowIndex < rowNum; rowIndex += 1) {
+        const arr: CalendarDateItem[] = [];
+        for (let columnIndex = 0; columnIndex < columnNum; columnIndex += 1) {
+          arr.push(generateCalendarItem(rowIndex, columnIndex));
+        }
+        calendar.push(arr);
+      }
     };
 
     /**
@@ -132,7 +165,7 @@ export default defineComponent({
       } else {
         currentDate.month += 1;
       }
-      getCurrentMonthLastDayAndFirstDayWeek(currentDate.year, currentDate.month);
+      generateCalendar();
     };
 
     /**
@@ -145,36 +178,24 @@ export default defineComponent({
       } else {
         currentDate.month -= 1;
       }
-      getCurrentMonthLastDayAndFirstDayWeek(currentDate.year, currentDate.month);
+      generateCalendar();
     };
-
-    /**
-     * 是不是下一个月
-     */
-    const isNextMonth = (rowIndex: number, columnIndex: number) => (columnIndex + 1 + (rowIndex * 7) - (currentMonthFirstDayWeek.value - 1)) > lastDay.value;
-
-    /**
-     * 是不是上一个月
-     */
-    const isPrevMonth = (rowIndex: number, columnIndex: number) => rowIndex === 0 && (columnIndex + 1) < currentMonthFirstDayWeek.value;
 
     /**
      * 点击某一项
-     * @param rowIndex
-     * @param columnIndex
+     * @param date
      */
-    const handleOnClick = (rowIndex: number, columnIndex: number) => {
-      activeIndex.columnIndex = columnIndex;
-      activeIndex.rowIndex = rowIndex;
-      if (isPrevMonth(rowIndex, columnIndex)) {
+    const handleOnClick = (date: CalendarDateItem) => {
+      if (date.prev) {
         handleTogglePrevMonth();
-      } else if (isNextMonth(rowIndex, columnIndex)) {
+      } else if (date.next) {
         handleToggleNextMonth();
       }
+      calendarActiveDay.value = date.day as number;
     };
 
     onBeforeMount(() => {
-      getCurrentMonthLastDayAndFirstDayWeek(currentDate.year, currentDate.month);
+      generateCalendar();
     });
 
     return {
@@ -182,12 +203,10 @@ export default defineComponent({
       columnNum,
       weeks,
       lastDay,
+      calendar,
       currentDate,
-      activeIndex,
+      calendarActiveDay,
       currentMonthFirstDayWeek,
-      computedCurrentDate,
-      isNextMonth,
-      isPrevMonth,
       handleToggleNextMonth,
       handleTogglePrevMonth,
       handleOnClick,
