747e81ee967b704f0ffa50891abc18e20331f27d
刘凯 <1175047471@qq.com>
2021-07-06 17:04:13 +0800

    feat: Table 固定表头功能实现

--- packages/table/src/table.vue
+++ packages/table/src/table.vue
@@ -1,9 +1,15 @@
 <template>
   <div class='ki-table-container' :style='{height: height + "px"}' ref='tableContainerRef'>
     <div class='ki-table-header'>
-      <table :cellspacing='0' :cellpadding='0' class='ki-table' :class='{border: border}' :style='{width: tableWidth + "px"}'>
+      <table
+        :cellspacing='0'
+        :cellpadding='0'
+        class='ki-table'
+        :class='{border: border}'
+        style='table-layout: fixed;'
+      >
         <colgroup>
-          <col v-for='column in columns' :key='column.key' :width='getColumnWidth(column)' />
+          <col v-for='column in columns' :key='column.key' />
         </colgroup>
         <thead>
           <tr>
@@ -16,9 +22,15 @@
       </table>
     </div>
     <div class='ki-table-body' :style='{height: height - 50 + "px"}' :class='{fixedHead: isFixedHead}'>
-      <table :cellspacing='0' :cellpadding='0' class='ki-table' :class='{border: border, stripe: stripe}' :style='{width: (isFixedHead ? tableWidth - 17 : tableWidth) + "px"}'>
+      <table
+        :cellspacing='0'
+        :cellpadding='0'
+        class='ki-table'
+        :class='{border: border, stripe: stripe}'
+        style='table-layout: fixed;'
+      >
         <colgroup>
-          <col v-for='column in columns' :key='column.key' :width='getColumnWidth(column)' />
+          <col v-for='column in columns' :key='column.key' />
         </colgroup>
         <tbody>
           <tr v-for='(item, index) in data' :key='item.id' :class='[rowClassName && rowClassName(item, index)]'>
@@ -41,7 +53,7 @@
 </template>
 
 <script lang='ts'>
-import {defineComponent, h, computed, ref, onMounted, onUnmounted} from 'vue';
+import { defineComponent, h, computed, ref } from 'vue';
 import type { PropType, VNode } from 'vue';
 import KiRender from '@/components/render';
 
@@ -74,7 +86,6 @@ export default defineComponent({
   },
   setup(props) {
     const tableContainerRef = ref<HTMLDivElement | null>(null);
-    const tableWidth = ref(0);
     // 是否固定表头
     const isFixedHead = computed(() => props.height && props.height > 0);
 
@@ -84,44 +95,11 @@ export default defineComponent({
       return cssClass;
     };
 
-    const getColumnWidth = (column: TableColumn) => {
-      let len = 0, width = 0;
-      props.columns.forEach((item) => {
-        if (item.width) {
-          width += item.width;
-        } else {
-          len += 1;
-        }
-      });
-      if (column.width) {
-        return column.width;
-      }
-      const columnWidth = (tableWidth.value - width) / len;
-      return columnWidth < 80 ? 80 : columnWidth;
-    };
-    const getTableWidth = () => {
-      let minWidth = 0;
-      props.columns.forEach((item) => {
-        minWidth = item.width ? minWidth + item.width : minWidth + 80;
-      });
-      if (tableContainerRef.value) {
-        const width = tableContainerRef.value.getBoundingClientRect().width;
-        tableWidth.value = width < minWidth ? minWidth : width;
-      }
-    }
-    onMounted(() => {
-      getTableWidth();
-      window.addEventListener('resize', getTableWidth);
-    });
-    onUnmounted(() => window.removeEventListener('resize', getTableWidth));
-
     return {
-      tableWidth,
       tableContainerRef,
       isFixedHead,
 
       getColumnClass,
-      getColumnWidth,
     };
   },
 });
